bdbf233f1d5d9a4342ff7e842a59ae3d7875a7a8
Sakesun <sakesun@gmail.com>
2026-05-24 20:01:03 +0700

    weekend report

--- reports/2026-06-22-APIM-Normal/api-only.py
+++ reports/2026-06-22-APIM-Normal/api-only.py
@@ -284,31 +284,31 @@ def _(
 
 @app.cell
 def _(WorkSet, filter_for):
-    ws = WorkSet(filter_for(2026, 5, 19, 21, 00))
+    ws = WorkSet(filter_for(2026, 5, 22, 21, 00))
+    return (ws,)
+
+
+@app.cell
+def _(ws):
+    ws.show_sources()
     return
 
 
 @app.cell
-def _():
-    # ws.show_sources()
+def _(ws):
+    ws.api.percentiles()
     return
 
 
 @app.cell
-def _():
-    # ws.api.percentiles()
+def _(ws):
+    ws.api.response_times()
     return
 
 
 @app.cell
-def _():
-    # ws.api.response_times()
-    return
-
-
-@app.cell
-def _():
-    # ws.api.error_rates()
+def _(ws):
+    ws.api.error_rates()
     return
 
 

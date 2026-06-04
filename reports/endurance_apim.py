3f9a76204b252011e25d48f049883dc9638124a8
Sakesun <sakesun@gmail.com>
2026-06-04 20:29:48 +0700

    patch

--- reports/endurance_apim.py
+++ reports/endurance_apim.py
@@ -31,6 +31,12 @@ def _(ws):
 
 @app.cell
 def _(ws):
+    ws.api.error_rates()
+    return
+
+
+@app.cell
+def _(ws):
     ws.api.timeline_tps()
     return
 
@@ -55,19 +61,89 @@ def _(ws):
 
 @app.cell
 def _(ws):
-    ws.ui.timeline_tps()
+    ws.get_ui("(label='CreateIndividualProfile.Occupation.AddOccupationInformation.Save') and success").timeline_response_times()
     return
 
 
 @app.cell
 def _(ws):
-    ws.ui.timeline_response_times()
+    import marimo as _mo
+    import scuda as _scuda
+    seven_hours = 7 * 60 * 60 * 1000
+    starting = ws.ui.query("select max(epoch_ms(timeStamp)) ts from src where timeStamp < timestamp '2026-05-30 18:00:00.000' at time zone 'Asia/Bangkok'")['ts'][0] - seven_hours
+    ending   = ws.ui.query("select min(epoch_ms(timeStamp)) ts from src where timeStamp > timestamp '2026-05-30 18:00:00.000' at time zone 'Asia/Bangkok'")['ts'][0] - seven_hours
+    period = ending - starting
+    _mo.sql(f"""
+      select {starting} starting
+           , {ending}   ending
+           , to_timestamp({starting}/1000) at time zone 'UTC' starting_ts
+           , to_timestamp({ending}  /1000) at time zone 'UTC' ending_ts
+           , {period}
+    """)
+    return period, starting
+
+
+@app.cell
+def _(ws):
+    import marimo as _mo
+    import scuda as _scuda
+    _mo.sql(_scuda.with_raw_src_from_files(ws.result_files, f"""
+      select min(to_timestamp(timeStamp/1000) AT TIME ZONE 'Asia/Bangkok') min_bkk
+           , max(to_timestamp(timeStamp/1000) AT TIME ZONE 'Asia/Bangkok') max_bkk
+           , epoch_ms(min(to_timestamp(timeStamp/1000) AT TIME ZONE 'Asia/Bangkok')) min_bkk_ts
+           , min(timeStamp)                                                          min_ts
+           , epoch_ms(max(to_timestamp(timeStamp/1000) AT TIME ZONE 'Asia/Bangkok')) max_bkk_ts
+           , max(timeStamp)                                                          max_ts
+           , (min_bkk_ts - min_ts)
+           , (max_bkk_ts - max_ts)
+        from src
+    """, where=_scuda.IS_UI))
+
+    return
+
+
+@app.cell
+def _(period, starting, ws):
+    import marimo as _mo
+    import scuda as _scuda
+    _mo.sql(_scuda.with_raw_src_from_files(ws.result_files, f"""
+      select timestamp: (timeStamp + {period + period})
+           , elapsed, label, responseCode, responseMessage, threadName
+           , dataType, success, failureMessage, bytes, sentBytes
+           , grpThreads, allThreads, URL, Latency, IdleTime, Connect
+        from src
+        where src.timeStamp > {starting - period - period}
+          and src.timeStamp < {starting - period}
+        order by timeStamp
+    """, where=_scuda.IS_UI))
+
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    _df = mo.sql(
+        f"""
+
+        """
+    )
     return
 
 
 @app.cell
 def _(ws):
-    ws.ui.error_rates()
+    ws.get_ui('true').ui.timeline_response_times()
+    return
+
+
+@app.cell
+def _(ws):
+    ws.get_ui('true').timeline_error_rates()
     return
 
 
@@ -75,6 +151,13 @@ def _(ws):
 def _(ws):
     ws.ui.percentiles()
     return
+
+
+@app.cell
+def _():
+    import marimo as mo
+
+    return (mo,)
 
 
 if __name__ == "__main__":
